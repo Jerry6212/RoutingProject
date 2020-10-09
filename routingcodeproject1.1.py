5e93556fafa861b60a6312cb674b19581282a3fa
Jerry Ploegert <jerryjploegert@gmail.com>
2020-10-09 13:32:47 -0500

    Hid API key

--- routingcodeproject1.1.py
+++ routingcodeproject1.1.py
@@ -4,8 +4,9 @@ import pandas as pd
 import urllib.request
 import json
 import numpy as np
-from tkinter import *
-api_key = "AIzaSyBhIrlJ_lHYKdwMRna4NAOtUPdpK40DQMo"
+import os
+
+api_key = os.environ.get('GM_API_KEY')
 gmaps = googlemaps.Client(key=api_key)
 
 
@@ -16,8 +17,9 @@ def get_dist(x, y):
 tech_lst = []
 tech_dict = {}
 
+
 class Technician:
-    def __init__(self, name, area, abilities, max_num, phone_num, home, cx_list, cx_dist = []):
+    def __init__(self, name, area, abilities, max_num, phone_num, home, cx_list, cx_dist=[]):
         self.name = name
         self.area = area
         self.abilities = abilities
@@ -29,6 +31,7 @@ class Technician:
         tech_dict[self.name] = self.name, self.area, self.abilities, self.max_num, self.phone_num, self.home, \
                                self.cx_list
         tech_lst.append(self)
+
     def initials(self):
         return list(self.name[0])[0], list(self.name.split()[1])[0]
 
@@ -49,11 +52,12 @@ class Customer:
         self.email = email
         self.phone = phone
         self.dist = dist
-        cx_dict[self.cx_name] = self.address, self.appliance_type, self.appliance_brand, self.appliance_issue,\
+        cx_dict[self.cx_name] = self.address, self.appliance_type, self.appliance_brand, self.appliance_issue, \
                                 self.date, self.time_frame, self.email, self.phone
 
     def cx_address(self):
         return self.address
+
 
 cx_lst = []
 cx_1 = Customer("jimmy john", "2303 ballantrae dr, colleyville, texas, 76034", "fridge", "samsung", "no cool", "7/7/20",
@@ -65,7 +69,8 @@ john_doe = Customer("John Doe", "506 Hettie st, Denton, 76209", "Refrigerator", 
 bill_noff = Customer("Bill Noff", "520 Kay st, argyle, tx, 76226", "washer", "LG", "Leak", "7/21/20", "8-10", None,
                      "000-000-0000")
 jack_doe = Customer("Jack Doe", "199 w Jones st, Krum, Tx", "washer", "Samsung", "not spinning", None, None, None, None)
-steve_edwards = Customer("Steve Edwards", "4017 Ringdove Way, Roanoke, Tx", "dryer", "Maytag", "no heat", None, None, None, None)
+steve_edwards = Customer("Steve Edwards", "4017 Ringdove Way, Roanoke, Tx", "dryer", "Maytag", "no heat", None, None,
+                         None, None)
 cx_lst.append(cx_1)
 cx_lst.append(rick_smith)
 cx_lst.append(john_doe)
@@ -78,19 +83,20 @@ Dale = Technician("Dale Nester", "Ft Worth", "All", 10, "111-222-3333", "4205 el
 addy_lst = []
 for cx in cx_lst:
     addy_lst.append(cx.cx_address())
-#for cx in cx_lst:
-    #if int(str(get_dist(Jerry.home, cx.address)).split(" ")[0]) < int(str(get_dist(Edwin.home, cx.address)).split(" ")[0]) and int(str(get_dist(Jerry.home, cx.address)).split(" ")[0]) < int(str(get_dist(Dale.home, cx.address)).split(" ")[0]):
-        #Jerry.cx_list.append(cx.cx_address())
-    #elif int(str(get_dist(Edwin.home, cx.address)).split(" ")[0]) < int(str(get_dist(Jerry.home, cx.address)).split(" ")[0]) and int(str(get_dist(Edwin.home, cx.address)).split(" ")[0]) < int(str(get_dist(Dale.home, cx.address)).split(" ")[0]):
-        #Edwin.cx_list.append(cx.cx_address())
-    #elif int(str(get_dist(Dale.home, cx.address)).split(" ")[0]) < int(str(get_dist(Jerry.home, cx.address)).split(" ")[0]) and int(str(get_dist(Dale.home, cx.address)).split(" ")[0]) < int(str(get_dist(Edwin.home, cx.address)).split(" ")[0]):
-        #Dale.cx_list.append(cx.cx_address())
+# for cx in cx_lst:
+# if int(str(get_dist(Jerry.home, cx.address)).split(" ")[0]) < int(str(get_dist(Edwin.home, cx.address)).split(" ")[0]) and int(str(get_dist(Jerry.home, cx.address)).split(" ")[0]) < int(str(get_dist(Dale.home, cx.address)).split(" ")[0]):
+# Jerry.cx_list.append(cx.cx_address())
+# elif int(str(get_dist(Edwin.home, cx.address)).split(" ")[0]) < int(str(get_dist(Jerry.home, cx.address)).split(" ")[0]) and int(str(get_dist(Edwin.home, cx.address)).split(" ")[0]) < int(str(get_dist(Dale.home, cx.address)).split(" ")[0]):
+# Edwin.cx_list.append(cx.cx_address())
+# elif int(str(get_dist(Dale.home, cx.address)).split(" ")[0]) < int(str(get_dist(Jerry.home, cx.address)).split(" ")[0]) and int(str(get_dist(Dale.home, cx.address)).split(" ")[0]) < int(str(get_dist(Edwin.home, cx.address)).split(" ")[0]):
+# Dale.cx_list.append(cx.cx_address())
 if len(tech_lst) == 1:
     for cx in cx_lst:
         tech_lst[0].cx_lst.append(cx.cx_address())
 elif len(tech_lst) == 2:
     for cx in cx_lst:
-        if int(str(get_dist(tech_lst[0].home, cx.address)).split(" ")[0]) < int(str(get_dist(tech_lst[1].home, cx.address)).split(" ")[0]):
+        if int(str(get_dist(tech_lst[0].home, cx.address)).split(" ")[0]) < int(
+                str(get_dist(tech_lst[1].home, cx.address)).split(" ")[0]):
             tech_lst[0].cx_lst.append(cx.cx_address())
         else:
             tech_lst[1].cx_lst.append(cx.cx_address())
@@ -109,6 +115,7 @@ elif len(tech_lst) == 3:
                 int(str(get_dist(tech_lst[1].home, cx.address)).split(" ")[0]):
             tech_lst[2].cx_list.append(cx.cx_address())
 
+
 def get_lat_long(tech_route):
     loc_long = []
     loc_lat = []
@@ -116,7 +123,7 @@ def get_lat_long(tech_route):
     results = []
     for location_str in tech_route:
         r = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address='%s'&key=%s"
-                        % (location_str, api_key))
+                         % (location_str, api_key))
         result = r.json()["results"]
         location = result[0]["geometry"]["location"]
         loc_long.append(location["lng"])
@@ -124,20 +131,18 @@ def get_lat_long(tech_route):
         formatted_addys.append(result[0]["formatted_address"])
         results.append(result)
 
-    df = pd.DataFrame({"address": tech_route, "latitude": loc_lat, "longitude": loc_long, "formatted_address": formatted_addys})
+    df = pd.DataFrame(
+        {"address": tech_route, "latitude": loc_lat, "longitude": loc_long, "formatted_address": formatted_addys})
     waypoints = []
     lat_long = list(zip(loc_lat, loc_long))
 
     for i in lat_long:
         waypoints.append(str(i).replace(" ", ""))
-    waypoints = str(waypoints).replace(", ", "|").replace("(", "").replace(")", "").replace("[", "").replace("]", "")\
+    waypoints = str(waypoints).replace(", ", "|").replace("(", "").replace(")", "").replace("[", "").replace("]", "") \
         .replace("'", "")
-
 
     origin = loc_lat[0], loc_long[0]
     origin = str(origin).replace(" ", "").replace("(", "").replace(")", "")
-
-
 
     destination = loc_lat[-1], loc_long[-1]
     destination = str(destination).replace(" ", "").replace("(", "").replace(")", "")
@@ -149,7 +154,7 @@ def get_lat_long(tech_route):
 
     for i in range(len(waypoints.split("|"))):
         route = ("https://maps.googleapis.com/maps/api/directions/json?language=en-US&units=imperial&origin={}"
-                "&destination={}&waypoints=optimize:true|{}&key={}").format(origin, destination, waypoints, api_key)
+                 "&destination={}&waypoints=optimize:true|{}&key={}").format(origin, destination, waypoints, api_key)
         route_result = urllib.request.urlopen(route)
         route_result_json = dict(json.loads(route_result.read()))
         distance.append(route_result_json["routes"][0]["legs"][i]["distance"]["text"])
@@ -158,19 +163,15 @@ def get_lat_long(tech_route):
         end_address.append(route_result_json["routes"][0]["legs"][i]["end_address"])
 
     organized_df = pd.DataFrame({"start_address": start_address, "end_address": end_address,
-                                "distance": distance, "duration": duration},
-                                 columns=["start_address", "end_address", "distance", "duration"])
+                                 "distance": distance, "duration": duration},
+                                columns=["start_address", "end_address", "distance", "duration"])
     optimized_route = str(organized_df["end_address"]).split("Name")[0].strip()
-
-
 
     return str(optimized_route).strip()
 
 
-
 for tech in tech_lst:
     print(tech.name + "\n" + get_lat_long(tech.cx_list))
-
 
 print(Jerry.cx_list)
 print(Dale.cx_list)
